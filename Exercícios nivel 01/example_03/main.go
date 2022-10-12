e09c5428e44d595410ac3954e652f2583692f927
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-10-12 09:51:09 -0300

    resolve example #03

--- Exercícios nivel 01/example_03/main.go
+++ Exercícios nivel 01/example_03/main.go
@@ -2,15 +2,12 @@ package main
 
 import "fmt"
 
-
+var x int = 42
+var y string = "James Bond"
+var z bool = true
 
 func main() {
-	var x int = 42
-	var y string = "James Bond"
-	var z bool = true
-
-	s := fmt.Sprintf(string(rune(x)), y, z)
+	s := fmt.Sprintf("%v\t%v\t%v", x, y, z)
 
 	fmt.Println(s)
-
 }
